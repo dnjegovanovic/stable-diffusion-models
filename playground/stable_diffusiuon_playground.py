63a34c96469743b78bfc567ed52a3d568244c970
dusannj <dusan.njegovnaovic95@gmail.com>
2023-09-13 22:53:40 +0200

    Visualize diffuzer steps.

--- playground/stable_diffusiuon_playground.py
+++ playground/stable_diffusiuon_playground.py
@@ -4,11 +4,17 @@ import torch.nn.functional as F
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 import matplotlib.pyplot as plt
-
+import itertools
+import math
+import mediapy as media
+from pathlib import Path
 import json
-with open('./token.json', 'r') as f:
-  data_token = json.load(f)
-  
+import numpy as np
+import torchvision
+
+with open("./token.json", "r") as f:
+    data_token = json.load(f)
+
 from huggingface_hub import login
 
 login(data_token["token"])
@@ -41,7 +47,9 @@ def create_pipe():
 
 def create_simple_img(pipe, prompt, img_name, generator=None, num_inference_steps=25):
     if num_inference_steps is not None:
-        data = pipe(prompt, generator=generator, num_inference_steps=num_inference_steps)
+        data = pipe(
+            prompt, generator=generator, num_inference_steps=num_inference_steps
+        )
     else:
         data = pipe(prompt, generator=generator)
     image = data.images[0]
@@ -49,15 +57,52 @@ def create_simple_img(pipe, prompt, img_name, generator=None, num_inference_step
     plt_show_image(image)
 
 
-def main():
-    pass
+def create_video(images, video_name):
+    shape = (480, 640)
+    output_path = f'./playground_imgs/{video_name}.mp4'
+    with media.VideoWriter(output_path, shape=shape, fps=10) as w:
+        for image in images:
+            w.add_image(image)
 
-
-if __name__ == "__main__":
+def simple_generation_diffuser_step_vis():
     generator = torch.Generator("cuda").manual_seed(1024)
 
     prompt1 = "a lovely cat running in the desert in Van Gogh style, trending art."
     prompt2 = "a sleeping cat enjoying the sunshine."
     prompt1 = "a lovely cat running in the desert in Van Gogh style, trending art."
     pipe = create_pipe()
-    create_simple_img(pipe, prompt2, "nfprompt_sunshine_interference_cat", generator)
+
+    # create_simple_img(pipe, prompt2, "nfprompt_sunshine_interference_cat", generator)
+
+    image_reservoir = []
+    latents_reservoir = []
+
+    # visualize diffuser steps
+    @torch.no_grad()
+    def plot_show_callback(i, t, latents):
+        latents_reservoir.append(latents.detach().cpu())
+        image = pipe.vae.decode(1 / 0.18215 * latents).sample
+        image = (image / 2 + 0.5).clamp(0, 1)
+        image = image.cpu().permute(0, 2, 3, 1).float().numpy()[0]
+        # plt_show_image(image)
+        plt.imsave(f"./playground_imgs/diff_steps/diffprocess_sample_{i:02d}.png", image)
+        image_reservoir.append(image)
+
+    @torch.no_grad()
+    def save_latents(i, t, latents):
+        latents_reservoir.append(latents.detach().cpu())
+
+    # Uncoment if you want to visualize diffuzer steps
+    # prompt = "a handsome cat dressed like Lincoln, trending art."
+    # with torch.no_grad():
+    #     image = pipe(prompt, callback=plot_show_callback).images[0]
+
+    # image.save(f"./playground_imgs/lovely_cat_lincoln.png")
+    
+    # create_video(np.array(image_reservoir), "difuser_steps")
+
+def simple_sampling_fun():
+    pass
+
+if __name__ == "__main__":
+    simple_generation_diffuser_step_vis()
